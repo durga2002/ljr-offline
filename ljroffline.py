511115e46458acd3a6daeb541c211534f980f326
vadim <durga2002@mail.ru>
2017-01-01 00:35:48 +0200

    index

--- ljroffline.py
+++ ljroffline.py
@@ -6,59 +6,62 @@ from html.parser import HTMLParser
 import re
 import sys
 
-user = 'user'
-rootUrl = 'http://lj.rossia.org/users/'+user
+# global definitions
+
+blogUrl = 'http://lj.rossia.org'
 saveDir = 'c:/tmp'
 imageDir = saveDir + '/images'
 badImportTitle = 'Imported event&nbsp;Original'
 firstYear = 2006
 lastYear = 2007
 
+
+
+# errors, warnings
 def report(s):
-    print('>>> LOG: ' + s)
+    print('>>> ' + s)
+    
+# progress    
+def log(msg):
+    print(msg)    
 
 
 ### index page ###########
 class indexPage:
-    def updateYear(self, year):
-        print('INXED: year ' + year)
+    def __init__(self, indexName=''):
+        if indexName:
+            try:
+                self.indexFile = open(file=indexName, mode='w', encoding = 'utf-8', buffering=1)
+            except OSError:
+                report('Cannot create index file')
+                sys.exit(-1)
+        else:
+            log('no target index name is spcified, using stdout')
+            self.indexFile = sys.stdout
+        print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> <html lang="en"> <head> <meta http-equiv="content-type" content="text/html; charset=utf-8"> <title>Index</title> </head> <body>', file = self.indexFile)
+    
+    def __del__(self):
+        print('<p></body> </html>', file = self.indexFile)
+        self.indexFile.close()
         
-    def updateMonth(self, month):
-        print('INXED:     month ' + month)
+    def addYear(self, year):
+        print('<p>year:', year, file = self.indexFile)
+        
+    def addMonth(self, month):
+        print('<p>&nbsp;&nbsp;month:', month, file = self.indexFile)
+
+    def addPost(self, title, link):
+        print('<p>&nbsp;&nbsp;&nbsp;&nbsp;<a href=\"' + link + '\">' + title + '</a>', file = self.indexFile)
+      
 
 #########################
-        
-class PostHeaderParserBad(HTMLParser):
-    
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.status = ''
-        self.postHeader = ''
-        self.headerFound = False
-    
-    def handle_endtag(self, tag):
-        if not self.headerFound and tag == 'center' and self.status == '':
-            print('end, ready_for_header')
-            self.status = 'ready_for_header'
-            
-    def handle_data(self, data):
-        if not self.headerFound and self.status == 'ready_for_header':
-            self.postHeader = data
-            self.status == ''
-            self.headerFound = True
-
-    def header(self):
-        if self.headerFound:
-            return self.postHeader[:100]
-        else:
-            return ''
-    
+       
 def title(text, url):
     found = re.search(r'<title>'+user+r': (.*)</title>', text, flags=re.IGNORECASE)
     if found:
-        title = found.group(1)
+        title = found.group(1).strip()
         if title == '' or title == badImportTitle:
-            title = '<image/video>'        
+            title = '[image/video]'        
     else:
         report(url + ': no TITLE tag')
         title = '???'
@@ -75,7 +78,8 @@ class MonthPageParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'a' and attrs[0][0] == 'href':
             self.link = attrs[0][1]
-            if re.match(r'http://lj.rossia.org/users/'+user+r'/\d*.html', self.link):
+            #if re.match(r'http://lj.rossia.org/users/'+user+r'/\d*.html', self.link):
+            if re.match(rootUrl + r'/\d*.html', self.link):
                 self.status = 'ready_for_header'
             
     def handle_data(self, data):
@@ -98,7 +102,7 @@ class ImageSaver(HTMLParser):
             imageUrl = attrs[0][1]
             imageName = imageUrl.split('/')[-1]
             if imageUrl.startswith('/'):
-                imageUrl = 'http://lj.rossia.org' + imageUrl
+                imageUrl = blogUrl + imageUrl
             targetImageName = self.targetDir + '/' + imageName
             if not os.path.exists(targetImageName):
                 try:
@@ -108,30 +112,8 @@ class ImageSaver(HTMLParser):
             elif not (imageUrl.startswith('/') or imageUrl.startswith('http://lj.rossia.org')): 
                 report('two images with the same name ' + imageUrl)
 
-index = indexPage()   
-#postParser = PostHeaderParser()    
-#monthParser = MonthPageParser()
-imageSaver = ImageSaver(imageDir)
-
-def main():
-    
-    os.makedirs(saveDir, exist_ok = True)
-    os.makedirs(imageDir, exist_ok = True)
-    
-    for year in reversed(range(firstYear, lastYear + 1)):
-        yearUrl = rootUrl + '/' + str(year)
-        try:
-            yearPage = urllib.request.urlopen(yearUrl)
-        except urllib.error.URLError as e:
-            log('URLError on ' + yearUrl)
-            continue
-        processYear(str(year))
-
-def log(msg):
-    print('Log:' + msg)
-
 def processYear(year):
-    index.updateYear(year)
+    index.addYear(year)
     for month in reversed(range(12)):
         processMonth(year, str(month+1))
 
@@ -142,32 +124,26 @@ def processPost(text):
     return text
     
 def processMonth(year, month):
-    index.updateMonth(month)
-    #print(rootUrl + '/' + year + '/' + month)
+    index.addMonth(month)
     
     try:
         monthPage = urllib.request.urlopen(rootUrl + '/' + year + '/' + month)
     except urllib.error.URLError as e:
-        log('URLError on ' + monthPage)
+        report('URLError on ' + monthPage)
       
     monthHtml = monthPage.read().decode('utf-8')
-    
-
-    #monthParser.feed(monthHtml)
-    #posts = monthParser.posts()
-    
-    posts = re.findall(r'http://lj.rossia.org/users/'+user+r'/\d*.html', monthHtml)
+    posts = re.findall(rootUrl + r'/\d*.html', monthHtml)
     
     for postUrl in posts:
         text = urllib.request.urlopen(postUrl).read().decode('utf-8', 'ignore')
-        
         text = processPost(text)
-        
         postFileName = postUrl.split('/')[-1]
-        with open(saveDir + '/' + postFileName, mode='w', encoding='utf_8') as file:
+        localFileName = saveDir + '/' + postFileName
+        with open(localFileName, mode='w', encoding='utf_8') as file:
             file.write(text)    
         subject = title(text, postUrl)
-        print(subject)
+        log(subject)
+        
         #if post[0] == '(no subject)':
             ##postParser.feed(text)
             ##title = postParser.header()
@@ -175,11 +151,29 @@ def processMonth(year, month):
             #title = post[0]
 
         # update index
-    
-    
+        index.addPost(subject, localFileName)
+      
+if len(sys.argv) < 2:
+    print('user name is not specified')
+    log('ERROR: user name is not specified')    
+    sys.exit(-1)
+user = sys.argv[1]
+rootUrl = blogUrl + '/users/' + user
 
-if __name__ == "__main__":
-    main()        
+index = indexPage('c:/tmp/index.html')   
+imageSaver = ImageSaver(imageDir)
+     
+os.makedirs(saveDir, exist_ok = True)
+os.makedirs(imageDir, exist_ok = True)
+
+for year in reversed(range(firstYear, lastYear + 1)):
+    yearUrl = rootUrl + '/' + str(year)
+    try:
+        yearPage = urllib.request.urlopen(yearUrl)
+    except urllib.error.URLError as e:
+        report('URLError on ' + yearUrl)
+        continue
+    processYear(str(year))    
 
 
 # !!! good index and logging
